9f9677a6bab0e078e6c989a71735d47768bf28f6
alicia <alimcbeal1387@gmail.com>
2024-06-21 16:13:49 +0100

    added a new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>My first app</h1>
-      <h2>By Alice</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <title><b><i>Lady Gaga</i></b></title>
+      <h1 id="title">Lady gaga , a great songwriter, singer, actress, philanthropist, dancer and fashion designer.</h1>
+
+      <h2>Brief History</h2>
+      <p>
+        Gaga was born on March 28, 1986 in Manhattan, New York City. By the age of 14, Gaga was performing at open mike nights in clubs and bars.
+        In 2008 Gaga released her first album 'The Fame' to lukewarm radio play; Gaga toured around Europe and in gay clubs in the US to promote the album.
+        Since then Gaga has gained numerous awards and nominations for a string of hits;
+        her first album spawned several more smash hits 'Paparazzi', 'Loveame' and 'Poker Face'.
+        In 2011 Gaga released her second full-length album 'Born this Way'.At the end of April 2012, Gaga's Born This Way kicked off in Korea
+
+        the tour would last 2 years and take the singer to every continent of the globe. Gaga wrote her third album, ARTPOP, released in 2013.
+        Gaga made her acting debut in Robert Rodriguez's Machete Kills (2013), the sequel to his 2010 film Machete, and also appeared in Rodriguez's sequel Sin City: A Dame to Kill For (2014).
+        In 2018, she starred with Bradley Cooper, who also directed, in A Star Is Born (2018).
+        Gaga received her first Academy Award nomination for Best Actress for the role.
       </p>
+
+      <p>For more information, please visit <a id="tribute-link" href="https://en.wikipedia.org/wiki/Lady_Gaga" target="_blank">Lady Gaga on Wikipedia</a></p>
+
+
+
+
     </>
   )
 }
