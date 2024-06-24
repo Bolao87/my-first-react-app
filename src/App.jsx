4dd4de602474a5c5271829520bfc1d950c7e601b
alicia <alimcbeal1387@gmail.com>
2024-06-24 11:23:57 +0100

    added a new para

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,25 @@ function App() {
       <title><b><i>Lady Gaga</i></b></title>
       <h1 id="title">Lady gaga , a great songwriter, singer, actress, philanthropist, dancer and fashion designer.</h1>
 
-      
+
       <img src="/Gaga.jpeg" alt="gaga singing in a beautiful blue dress" />
-    
+
       <h2> A Brief History</h2>
-    
-    <p>
-        Gaga was born on March 28, 1986 in Manhattan, New York City. By the age of 14, Gaga was performing at open mike nights in clubs and bars.
+
+      <p>
+        Gaga was born on March 28, 1986 in Manhattan, New York City.  By the age of 14, Gaga was performing at open mike nights in clubs and bars.
         In 2008 Gaga released her first album 'The Fame' to lukewarm radio play; Gaga toured around Europe and in gay clubs in the US to promote the album.
         Since then Gaga has gained numerous awards and nominations for a string of hits;
         her first album spawned several more smash hits 'Paparazzi', 'Loveame' and 'Poker Face'.
-        In 2011 Gaga released her second full-length album 'Born this Way'.At the end of April 2012, Gaga's Born This Way kicked off in Korea
+        In 2011 Gaga released her second full-length album 'Born this Way'.  At the end of April 2012, Gaga's Born This Way kicked off in Korea
 
-        the tour would last 2 years and take the singer to every continent of the globe. Gaga wrote her third album, ARTPOP, released in 2013.
+
+      </p>
+      <p>  the tour would last 2 years and take the singer to every continent of the globe. Gaga wrote her third album, ARTPOP, released in 2013.
         Gaga made her acting debut in Robert Rodriguez's Machete Kills (2013), the sequel to his 2010 film Machete, and also appeared in Rodriguez's sequel Sin City: A Dame to Kill For (2014).
         In 2018, she starred with Bradley Cooper, who also directed, in A Star Is Born (2018).
         Gaga received her first Academy Award nomination for Best Actress for the role.
-      </p>
+        </p>
 
       <p>For more information, please visit <a id="tribute-link" href="https://en.wikipedia.org/wiki/Lady_Gaga" target="_blank">Lady Gaga on Wikipedia</a></p>
 
