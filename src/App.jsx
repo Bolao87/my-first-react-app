5f2b5303b61c758515dd96e5ffe36ad5cb01c079
alicia <alimcbeal1387@gmail.com>
2024-06-25 13:30:32 +0100

    added some words to another para

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,15 @@ function App() {
         In 2008 Gaga released her first album 'The Fame' to lukewarm radio play; Gaga toured around Europe and in gay clubs in the US to promote the album.
         Since then Gaga has gained numerous awards and nominations for a string of hits;
         her first album spawned several more smash hits 'Paparazzi', 'Loveame' and 'Poker Face'.
-        In 2011 Gaga released her second full-length album 'Born this Way'.  At the end of April 2012, Gaga's Born This Way kicked off in Korea
-
+        In 2011 Gaga released her second full-length album 'Born this Way'.
 
       </p>
-      <p>  the tour would last 2 years and take the singer to every continent of the globe. Gaga wrote her third album, ARTPOP, released in 2013.
+      <p>  At the end of April 2012, Gaga's Born This Way kicked off in Korea
+        the tour would last 2 years and take the singer to every continent of the globe. Gaga wrote her third album, ARTPOP, released in 2013.
         Gaga made her acting debut in Robert Rodriguez's Machete Kills (2013), the sequel to his 2010 film Machete, and also appeared in Rodriguez's sequel Sin City: A Dame to Kill For (2014).
         In 2018, she starred with Bradley Cooper, who also directed, in A Star Is Born (2018).
         Gaga received her first Academy Award nomination for Best Actress for the role.
-        </p>
+      </p>
 
       <p>For more information, please visit <a id="tribute-link" href="https://en.wikipedia.org/wiki/Lady_Gaga" target="_blank">Lady Gaga on Wikipedia</a></p>
 
