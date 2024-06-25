e10b6b473ee8670138f5edc0731523779ae8318f
alicia <alimcbeal1387@gmail.com>
2024-06-25 16:15:05 +0100

    added a new music link

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,18 @@ function App() {
       <h2> A Brief History</h2>
 
       <p>
-        Gaga was born on March 28, 1986 in Manhattan, New York City.  By the age of 14, Gaga was performing at open mike nights in clubs and bars.
-        In 2008 Gaga released her first album 'The Fame' <a id="tribute-link" href="https://www.youtube.com/watch?v=8mZb6pvUNmk" target="_blank">Lady Gaga first song</a> on lukewarm radio play; Gaga toured around Europe and in gay clubs in the US to promote the album.
-        Since then Gaga has gained numerous awards and nominations for a string of hits;
+         Lady Gaga was born on March 28, 1986 in Manhattan, New York City.  By the age of 14, Gaga was performing at open mike nights in clubs and bars.
+        In 2008 Gaga released her first album 'The Fame' <a id="tribute-link" href="https://www.youtube.com/watch?v=8mZb6pvUNmk" target="_blank">" The Fame "</a> on lukewarm radio play; Gaga toured around Europe and in gay clubs in the US to promote the album.
+        Since then Lady Gaga has gained numerous awards and nominations for a string of hits;
         her first album spawned several more smash hits 'Paparazzi', 'Loveame' and 'Poker Face'.
         In 2011 Gaga released her second full-length album 'Born this Way'.
 
       </p>
       <p>  At the end of April 2012, Gaga's Born This Way kicked off in Korea
-        the tour would last 2 years and take the singer to every continent of the globe. Gaga wrote her third album, ARTPOP, released in 2013.
+        the tour would last 2 years and take the singer to every continent of the globe. Lady Gaga wrote her third album, ARTPOP, released in 2013  <a id="tribute-link" href="https://www.youtube.com/watch?v=9d5j1klMYqw" target="_blank">"ARTPOP"</a>.
         Gaga made her acting debut in Robert Rodriguez's Machete Kills (2013), the sequel to his 2010 film Machete, and also appeared in Rodriguez's sequel Sin City: A Dame to Kill For (2014).
         In 2018, she starred with Bradley Cooper, who also directed, in A Star Is Born (2018).
-        Gaga received her first Academy Award nomination for Best Actress for the role.
+       Lady_Gaga Gaga received her first Academy Award nomination for Best Actress for the role.
       </p>
 
       <p>For more information, please visit <a id="tribute-link" href="https://en.wikipedia.org/wiki/Lady_Gaga" target="_blank">Lady Gaga on Wikipedia</a></p>
